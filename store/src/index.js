ceef3a11516469ec83c6d7d1571d1c03cacf2e09
Travis Willey <traviswilley24@yahoo.com>
2022-08-05 23:04:55 -0700

    save

--- store/src/index.js
+++ store/src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter as Router } from 'react-router-dom';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap-icons/font/bootstrap-icons.css';
@@ -7,4 +8,8 @@ import './components/main.css';
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<App />);
+root.render(
+  <Router>
+    <App />
+  </Router>
+);
