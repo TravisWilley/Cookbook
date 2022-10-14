aa820b04c4d2365b1fbebcc82ddda46401271209
Travis Willey <traviswilley24@yahoo.com>
2022-10-14 01:35:12 -0700

    RedditClone

---
+++ portfolio/src/MenuBar/MenuBar.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './MenuBar.css';
+
+export default function MenuBar() {
+  return (
+    <div className="menuBar">
+      <ul>
+        <li>Travis Willey</li>
+        <li>Software Developer</li>
+      </ul>
+    </div>
+  );
+}
---
+++ portfolio/src/Resume/Resume.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default function Resume() {
+  return (
+    <div>
+      <form>
+        <label>Travis Willey</label>
+      </form>
+    </div>
+  );
+}
