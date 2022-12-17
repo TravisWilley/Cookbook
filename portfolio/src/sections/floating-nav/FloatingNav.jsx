d91d81c8ab6eb8fa0fe68e77130bf0ccbe44881e
Travis Willey <traviswilley24@yahoo.com>
2022-12-17 10:38:43 -0800

    portfolio

---
+++ portfolio/src/sections/floating-nav/FloatingNav.jsx
@@ -0,0 +1,6 @@
+import './floatingnav.css';
+const FloatingNav = () => {
+  return <section id="floating_nav"></section>;
+};
+
+export default FloatingNav;
---
+++ portfolio/src/sections/navbar/Navbar.jsx
@@ -0,0 +1,13 @@
+import './navbar.css';
+
+import React from 'react';
+
+const Navbar = () => {
+  return (
+    <nav>
+      <h2>Navbar</h2>
+    </nav>
+  );
+};
+
+export default Navbar;
